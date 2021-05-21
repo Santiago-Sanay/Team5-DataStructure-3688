e7d6245331c1f297fe51b651a901467a0acf63e5
Alex Santiago Paguay Flores <75266819+alexpaguay1991@users.noreply.github.com>
2021-05-20 21:15:55 -0500

    "calculator .cpp"

--- I-PARCIAL/Tarea 1 Basic Operations/Operaciones/Operaciones/Calculator.cpp
+++ I-PARCIAL/Tarea 1 Basic Operations/Operaciones/Operaciones/Calculator.cpp
@@ -1,33 +1,42 @@
-
-
+/** UNIVERSIDAD DE LAS FUERZAS ARMADAS "ESPE"
+*			INGENIERIA SOFTWARE
+*
+*AUTORES : YULLIANA ROMAN
+*JUNIOR JURADO
+*TEHO ROSERO
+*ALEX PAGUAY
+*SANTIAGO SAÑAY
+*TEMA: CREACION DE UN PROGRAMA DE OPERACIONES BASICAS(SUMA, RESTA, MULTIPLICAION, DIVISION) CON TEMPLATES<>
+*FECHA DE CREACION : 19 DE MAYO DEL 2021
+*FECHA DE MODIFICACION: 20 DE MAYO 2021
+*/
 template <typename T>
-T add()
+T Calculator<T>::add(Number<T>& op)
 {
-	return number_one + number_two;
+	return  op.get_number_one() + op.get_number_two();
 }
 
 template <typename T>
-T subtract()
+T Calculator<T>::subtract(Number<T>& op)
 {
-	return number_one - number_two;
-}
-
-
-template <typename T>
-T multiply()
-{
-	return number_one * number_two;
+	return op.get_number_one() - op.get_number_two();
 }
 
 template <typename T>
-T divide()
+T Calculator<T>::multiply(Number<T>& op)
 {
-	if (number_two == 0)
+	return op.get_number_one() * op.get_number_two();
+}
+
+template <typename T>
+T Calculator<T>::divide(Number<T>& op)
+{
+	if (op.get_number_two() == 0)
 	{
-		throw "Parametros Invalidos";
+		return 0;
 	}
 	else
 	{
-		return number_one / number_two;
+		return op.get_number_one() / op.get_number_two();
 	}
 }
--- I-PARCIAL/Tarea 1 Basic Operations/Operaciones/Operaciones/Calculator.h
+++ I-PARCIAL/Tarea 1 Basic Operations/Operaciones/Operaciones/Calculator.h
@@ -1,6 +1,20 @@
+/** UNIVERSIDAD DE LAS FUERZAS ARMADAS "ESPE"
+*			INGENIERIA SOFTWARE
+*
+*AUTORES : YULLIANA ROMAN
+*JUNIOR JURADO
+*TEHO ROSERO
+*ALEX PAGUAY
+*SANTIAGO SAÑAY
+*TEMA: CREACION DE UN PROGRAMA DE OPERACIONES BASICAS(SUMA, RESTA, MULTIPLICAION, DIVISION) CON TEMPLATES<>
+*FECHA DE CREACION : 19 DE MAYO DEL 2021
+*FECHA DE MODIFICACION: 20 DE MAYO 2021
+*/
 #pragma once
+#include "Number.h"
+
 template <typename T>
-class Utils
+class Calculator
 {
 public:
 	/**
@@ -8,29 +22,28 @@ public:
 	 *
 	 * @return the sum.
 	 */
-
-	static T add();
+	static T add(Number<T>&);
 
 	/**
 	 * @brief Member function to subtract two numbers
 	 *
 	 * @return the subtraction
 	 */
-	static T subtract();
+	static T subtract(Number<T>&);
 
 	/**
 	 * @brief Member function to multiply two numbers
 	 *
 	 * @return the multiplication
 	 */
-	static T multiply();
+	static T multiply(Number<T>&);
 
 	/**
 	 * @brief Member function to divide two numbers
 	 *
 	 * @return the divide
 	 */
-	static T divide();
+	static T divide(Number<T>&);
 };
 
-#include "utils.cpp"
+#include "Calculator.cpp"
