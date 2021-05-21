56b3095a5bbf04e011dac7e9dcea393f2c9dc410
Santigo-Sanay <hpss30@hotmail.com>
2021-05-20 21:07:32 -0500

    fix Number.cpp

--- I-PARCIAL/Tarea 1 Basic Operations/Operaciones/Operaciones/Number.cpp
+++ I-PARCIAL/Tarea 1 Basic Operations/Operaciones/Operaciones/Number.cpp
@@ -1,22 +1,40 @@
 
+/** UNIVERSIDAD DE LAS FUERZAS ARMADAS "ESPE"
+*			INGENIERIA SOFTWARE
+*
+*AUTORES : YULLIANA ROMAN
+*JUNIOR JURADO
+*TEHO ROSERO
+*ALEX PAGUAY
+*SANTIAGO SAÑAY
+*TEMA: CREACION DE UN PROGRAMA DE OPERACIONES BASICAS(SUMA, RESTA, MULTIPLICAION, DIVISION) CON TEMPLATES<>
+*FECHA DE CREACION : 19 DE MAYO DEL 2021
+*FECHA DE MODIFICACION: 20 DE MAYO 2021
+*/
 
 template <typename T>
-void Operaciones<T>::set_number_one(T)
+Number<T>::Number(T x, T y) :number_one(x), number_two(y)
+{
+
+}
+template <typename T>
+void Number<T>::set_number_one(T)
 {
 	(*this).number_one = T;
 }
 template <typename T>
-void Operaciones<T>::set_number_two(T)
+void Number<T>::set_number_two(T)
 {
 	(*this).number_two = T;
 }
 template <typename T>
-T Operaciones<T>::get_number_one()
+T Number<T>::get_number_one() const noexcept
 {
 	return number_one;
 }
 template <typename T>
-T Operaciones<T>::get_number_two()
+
+T Number<T>::get_number_two() const noexcept
 {
 	return number_two;
 }
