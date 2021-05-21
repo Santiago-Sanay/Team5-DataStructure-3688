37550ad4c3d7b8f4fb3f00aa5a90490e50fd8464
YullianaRoman <yulianaroman376@gmail.com>
2021-05-20 21:31:10 -0500

    Fix operaciones.cpp

--- I-PARCIAL/Tarea 1 Basic Operations/Operaciones/Operaciones/Operaciones.cpp
+++ I-PARCIAL/Tarea 1 Basic Operations/Operaciones/Operaciones/Operaciones.cpp
@@ -1,18 +1,59 @@
-
+/** UNIVERSIDAD DE LAS FUERZAS ARMADAS "ESPE"
+*			INGENIERIA SOFTWARE
+*
+*AUTORES : YULLIANA ROMAN
+*JUNIOR JURADO
+*TEHO ROSERO
+*ALEX PAGUAY
+*SANTIAGO SAÑAY
+*TEMA: CREACION DE UN PROGRAMA DE OPERACIONES BASICAS(SUMA, RESTA, MULTIPLICAION, DIVISION) CON TEMPLATES<>
+*FECHA DE CREACION : 19 DE MAYO DEL 2021
+*FECHA DE MODIFICACION: 20 DE MAYO 2021
+*/
 #include <iostream>
-#include "OperacionesBasicas.h"
+#include "Number.h"
+#include "Calculator.h"
 
 int32_t main()
 {
+
 	double number_one = 7;
 	double number_two = 8;
-	Operaciones<double> op(number_one, number_two);
+	Number<double> op(number_one, number_two);
 
-	std::cout << op.add() << std::endl;
+	std::cout << "La suma entre "
+		<< op.get_number_one() << " y "
+		<< op.get_number_two() << " es: "
+		<< Calculator<double>::add(op)
+		<< std::endl;
 
-	int n = 7;
+	int n = 15;
 	int m = 8;
-	Operaciones<int> op2{ n, m };
-	std::cout << op2.subtract() << std::endl;
+	Number<int> op2{ n, m };
+	std::cout << "La resta entre "
+		<< op2.get_number_one() << " y "
+		<< op2.get_number_two() << " es: "
+		<< Calculator<int>::subtract(op2)
+		<< std::endl;
+
+	float a = 20.5F;
+	float b = 7.6F;
+	Number<float> op3{ a, b };
+	std::cout << "La multiplicacion entre "
+		<< op3.get_number_one() << " y "
+		<< op3.get_number_two() << " es: "
+		<< Calculator<float>::multiply(op3)
+		<< std::endl;
+
+	int16_t c = 9;
+	int16_t d = 9;
+	Number<int16_t> op4{ c, d };
+	std::cout << "La division entre "
+		<< op4.get_number_one() << " y "
+		<< op4.get_number_two() << " es: "
+		<< Calculator<int16_t>::divide(op4)
+		<< std::endl;
+
+
 	return EXIT_SUCCESS;
 }
