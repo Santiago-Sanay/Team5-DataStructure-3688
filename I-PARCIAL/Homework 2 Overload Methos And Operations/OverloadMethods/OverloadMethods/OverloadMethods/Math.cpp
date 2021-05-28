53cd1d5cd840f435d37164b90deb31a108908559
Santigo-Sanay <hpss30@hotmail.com>
2021-05-27 20:30:13 -0500

    Abstrac

--- I-PARCIAL/Homework 2 Overload Methos And Operations/OverloadMethods/OverloadMethods/OverloadMethods/Math.cpp
+++ I-PARCIAL/Homework 2 Overload Methos And Operations/OverloadMethods/OverloadMethods/OverloadMethods/Math.cpp
@@ -23,15 +23,48 @@ Vector<double> Math::add(Vector<double>&, Vector<float>&)
 }
 
 //overload subtract
-Vector<int> Math::subtract(Vector<int>&, Vector<int>&)
+Vector<int> Math::subtract(Vector<int>&u, Vector<int>&v)
 {
+	Vector<int> result_vector{};
+	int x = u.get_x() - v.get_x();
+	result_vector.set_x(x);
+
+	int y = u.get_y() - v.get_y();
+	result_vector.set_y(y);
+
+	int z = u.get_z() - v.get_z();
+	result_vector.set_z(z);
+
+	return result_vector;
 }
 
-Vector<float> Math::subtract(Vector<float>&, Vector<int>&)
+Vector<float> Math::subtract(Vector<float>&u, Vector<int>&v)
 {
+	Vector<float> result_vector{};
+	float x = u.get_x() - static_cast<float>(v.get_x());
+	result_vector.set_x(x);
+
+	float y = u.get_y() - static_cast<float>(v.get_y());
+	result_vector.set_y(y);
+
+	float z = u.get_z() - static_cast<float>(v.get_z());
+	result_vector.set_z(z);
+
+	return result_vector;
 }
 Vector<double> Math::subtract(Vector<double>&, Vector<float>&)
 {
+	Vector<double> result_vector{};
+	double x = u.get_x() - static_cast<double>(v.get_x());
+	result_vector.set_x(x);
+
+	double y = u.get_y() - static_cast<double>(v.get_y());
+	result_vector.set_y(y);
+
+	double z = u.get_z() - static_cast<double>(v.get_z());
+	result_vector.set_z(z);
+
+	return result_vector;
 }
 
 //Overload Scalar Product
