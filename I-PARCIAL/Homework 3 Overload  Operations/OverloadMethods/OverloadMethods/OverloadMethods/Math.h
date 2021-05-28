2e9d68c7df6f9bb02e36825e3063d101a70af8d8
Junior Stalin <s_stalin999@hotmail.com>
2021-05-27 22:30:57 -0500

    Homework 3 Overload Operations

--- I-PARCIAL/Homework 3 Overload  Operations/OverloadMethods/OverloadMethods/OverloadMethods/Math.h
+++ I-PARCIAL/Homework 3 Overload  Operations/OverloadMethods/OverloadMethods/OverloadMethods/Math.h
@@ -6,28 +6,26 @@ class Math
 {
 
 public:
-	// overload add
-	static Vector<int> add(Vector<int>&, Vector<float>&);
-	static Vector<float> add(Vector<float>&, Vector<int>&);
-	static Vector<double> add(Vector<double>&, Vector<float>&);
+	//Theo
+	static Vector operator+(Vector&, Vector&);
+	static Vector operator-(Vector&, Vector&);
+	static double operator*(Vector&, Vector&);
+	//Yuli
+	static Vector operator++(Vector&);
+	static Vector operator--(Vector&);
+	static double operator/=(Vector&, Vector&);
+	//Santiago
+	static Vector operator~(Vector&);
+	static Vector operator>(Vector&, Vector&);
+	static Vector operator<(Vector&, Vector&);
+	//Alex
+	static bool operator==(Vector&, Vector&);
+	static Vector operator^(Vector&, int&);
+	static Vector operator!=(Vector&, Vector&);
+	//Junior
+	static int operator<=(Vector&, Vector&);
+	static Vector operator>=(Vector&, Vector&);
+	static Vector operator<<=(Vector&, Vector&);
 
-	//overload subtract
-	static Vector<int> subtract(Vector<int>&, Vector<int>&);
-	static Vector<float> subtract(Vector<float>&, Vector<int>&);
-	static Vector<double> subtract(Vector<double>&, Vector<float>&);
 
-	//Overload Scalar Product
-	static int scalar_product(Vector<int>&, Vector<int>&);
-	static float scalar_product(Vector<float>&, Vector<int>&);
-	static double scalar_product(Vector<double>&, Vector<float>&);
-
-	//Overload vector product..
-	static Vector<int> vector_product(Vector<int>&, Vector<int>&);
-	static Vector<float> vector_product(Vector<float>&, Vector<int>&);
-	static Vector<double> vector_product(Vector<double>&, Vector<float>&);
-
-	//scalar by a vector
-	static Vector<int> scalar_by_a_vector(Vector<int>&, Vector<int>&);
-	static Vector<float> scalar_by_a_vector(Vector<float>&, Vector<int>&);
-	static Vector<double> scalar_by_a_vector(Vector<double>&, Vector<float>&);
 };
