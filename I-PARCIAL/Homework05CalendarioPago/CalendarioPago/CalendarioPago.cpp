848ec8ac15cd685c10939d6fba82221441a826a7
ImTheo <theomrosero@gmail.com>
2021-06-01 21:57:00 -0500

    correcciones

--- I-PARCIAL/Homework05CalendarioPago/CalendarioPago/CalendarioPago.cpp
+++ I-PARCIAL/Homework05CalendarioPago/CalendarioPago/CalendarioPago.cpp
@@ -1,3 +1,17 @@
+/** UNIVERSIDAD DE LAS FUERZAS ARMADAS "ESPE"
+*			INGENIERIA SOFTWARE
+*
+*
+*@author THEO ROSERO
+*@author YULLIANA ROMAN
+*@author JUNIOR JURADO
+*@author ALEX PAGUAY
+*@author SANTIAGO SA�AY
+*TEMA: PARAMETRO POR REFERENCIA
+*FECHA DE CREACION : 29 DE MAYO DE 2021
+*FECHA DE MODIFICACION: 1 DE MAYO 2021
+*/
+
 // CalendarioPago.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
 //
 
