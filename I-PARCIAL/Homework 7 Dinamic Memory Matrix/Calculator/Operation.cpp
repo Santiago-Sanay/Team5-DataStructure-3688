8aec38f781d86e4260c4cd291bd32ab506a8168a
Alex Santiago Paguay Flores <75266819+alexpaguay1991@users.noreply.github.com>
2021-06-03 22:35:24 -0500

    "operation .cpp"

---
+++ I-PARCIAL/Homework 7 Dinamic Memory Matrix/Calculator/Operation.cpp
@@ -0,0 +1,56 @@
+#include "Operation.h"
+#include <conio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "Matrix.h"
+#include <iostream>
+using namespace std;
+
+int** Operation::segmentar(int dim) {
+    Matrix matrix;
+    int j;
+    matrix.set_matrix((int**)malloc(dim * sizeof(int*)));    
+    for (j = 0; j < dim; j++) {
+        *(matrix.get_matrix() + j) = (int*)malloc(dim * sizeof(int));
+    }
+    return matrix.get_matrix();
+}
+void Operation::encerar(Matrix matrix, int dim) {
+    int i, j;
+    for (i = 0; i < dim; i++)
+        for (j = 0; j < dim; j++)
+            *(*(matrix.get_matrix() + i) + j) = 0;
+}
+void Operation::ingresar(Matrix matrix, int dim) {
+    int i, j;
+    cout << "Ingrese los datos:" << endl;
+    for (i = 0; i < dim; i++)
+        for (j = 0; j < dim; j++) {
+            cout << "[" << i + 1 << "]" << "[" << j + 1 << "] :";
+            cin>>(*(*(matrix.get_matrix() + i) + j));
+        }
+}
+void Operation::calcular(Matrix matrix_1, Matrix matrix_2, Matrix matrix_r, int dim) {
+    int i, j, k;
+    for (i = 0; i < dim; i++) {
+        for (j = 0; j < dim; j++) {
+            for (k = 0; k < dim; k++) {
+                *(*(matrix_r.get_matrix() + i) + j) = *(*(matrix_r.get_matrix() + i) + j) + *(*(matrix_1.get_matrix() + i) + k) * *(*(matrix_2.get_matrix() + k) + j);
+            }
+        }
+    }
+}
+void Operation::imprimir(Matrix matrix, int dim) {
+    int i, j;
+    printf("\n");
+    for (i = 0; i < dim; i++) {
+        for (j = 0; j < dim; j++) {
+            printf("%d", *(*(matrix.get_matrix() + i) + j));
+            printf("%*s", j + 5, "");
+        }
+        printf("\n");
+    }
+
+}
+
+
