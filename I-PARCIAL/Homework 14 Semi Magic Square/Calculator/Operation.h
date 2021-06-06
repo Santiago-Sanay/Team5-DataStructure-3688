74ffb098237f5f44b21828fa8ade4f1cb3ff1513
Alex Santiago Paguay Flores <75266819+alexpaguay1991@users.noreply.github.com>
2021-06-06 17:00:32 -0500

    "semi magic square"

---
+++ I-PARCIAL/Homework 14 Semi Magic Square/Calculator/Operation.h
@@ -0,0 +1,94 @@
+﻿/** UNIVERSIDAD DE LAS FUERZAS ARMADAS "ESPE"
+*			INGENIERIA SOFTWARE
+*
+*
+*@author THEO ROSERO
+*@author YULLIANA ROMAN
+*@author JUNIOR JURADO
+*@author ALEX PAGUAY
+*@author SANTIAGO SA�AY
+*TEMA: PARAMETRO POR REFERENCIA
+*FECHA DE CREACION : 1 DE JUNIO DE 2021
+*FECHA DE MODIFICACION: 3 DE JUNIO 2021
+*/
+
+#pragma once
+#include "Matrix.h"
+
+class Operation
+{
+public:
+	/**
+	 * @brief Reserva memoria para la matriz
+	 * 
+	 * @param longitud 
+	 * @return la dirreccion de una matriz 
+	 */
+	int **segmentar(int);
+
+	/**
+	 * @brief Inicializa la matriz en cero
+	 * 
+	 * @param matriz_a 
+	 * @param longitud 
+	 */
+	void encerar(Matrix matriz_a, int longitud);
+
+	/**
+	 * @brief Asigna Valores a la matriz
+	 * 
+	 * @param matriz_a
+	 * @param longitud 
+	 */
+	void ingresar(Matrix, int);
+
+	/**
+	 * @brief  Multiplicacion de Matrices
+	 * 
+	 * @param _matrix_a 
+	 * @param _matrix_b 
+	 * @param _matrix_c 
+	 * @param longitud
+	 */
+	void calcular(Matrix, Matrix, Matrix, int);
+
+	/**
+	 * @brief Impresion de la matriz
+	 * 
+	 * @param _matrix_a 
+	 * @param longitud 
+	 */
+	void imprimir(Matrix, int);
+
+	/**
+	 * @brief Copia los datos de una matriz
+	 * 
+	 * @param _matrix_a 
+	 * @param _matrix_b 
+	 * @param longitud
+	 */
+	void copiar_matriz(Matrix, Matrix, int);
+
+	/**
+	 * @brief  Calcula la potencia de una matriz
+	 * 
+	 * @param _matrix_a 
+	 * @param _matrix_b 
+	 * @param longitud
+	 */
+	void calcular_potencia(Matrix, Matrix, int, int);
+	/**
+	 * @brief Llena matriz con datos numericos sin que sus diagonales sean iguales
+	 *
+	 * @param matriz_a
+	 * @param longitud
+	 */
+	void semi_magic_square(Matrix matriz_a, int longitud);
+	/**
+	 * @brief Intercambia filas y columnas
+	 *
+	 * @param matriz_a
+	 * @param longitud
+	 */
+	void permutate(Matrix matriz_a, int longitud);
+};
---
+++ I-PARCIAL/Homework 14 Semi Magic Square/Calculator/Semi_magic_square.cpp
@@ -0,0 +1,40 @@
+﻿/** UNIVERSIDAD DE LAS FUERZAS ARMADAS "ESPE"
+*			INGENIERIA SOFTWARE
+*
+*
+*@author THEO ROSERO
+*@author YULLIANA ROMAN
+*@author JUNIOR JURADO
+*@author ALEX PAGUAY
+*@author SANTIAGO SA�AY
+*TEMA: PARAMETRO POR REFERENCIA
+*FECHA DE CREACION : 1 DE JUNIO DE 2021
+*FECHA DE MODIFICACION: 3 DE JUNIO 2021
+*/
+
+#include <iostream>
+#include "Operation.h"
+#include "Matrix.h"
+using namespace std;
+
+int main()
+{
+    int size, pot;
+    Operation op;
+    Matrix matrix_1, matrix_2, matrix_r;
+    std::cout << "CUADRADO SEMI MAGICO\n";
+    cout << "Ingrese la dimension impar:";
+    cin >> size;
+    while(size%2==0){
+        cout << "Ingrese la dimension impar:";
+        cin >> size;
+    }
+    
+    matrix_1.set_matrix(op.segmentar(size));
+    
+    op.encerar(matrix_1, size);
+    op.semi_magic_square(matrix_1, size);
+    
+    op.imprimir(matrix_1, size);
+}
+
