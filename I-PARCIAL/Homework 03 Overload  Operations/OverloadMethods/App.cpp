5c93c519884b962d29f709fd851edf0fbd8a3e5e
Junior Stalin <s_stalin999@hotmail.com>
2021-06-18 08:56:01 -0500

    Add Operation

--- I-PARCIAL/Homework 03 Overload  Operations/OverloadMethods/App.cpp
+++ I-PARCIAL/Homework 03 Overload  Operations/OverloadMethods/App.cpp
@@ -19,43 +19,73 @@
 
 void Aplication::run()
 {
-	Vector* v = new Vector[]{ {7,4,5 }, { 6,7,8 } };
-	Math m;
-	Vector v3 = m.operator+(v);
+	std::regex pattern("(\\s*\\(\\s*([+-]?([0-9]*[.])?[0-9]+)\\s*\\,\\s*([+-]?([0-9]*[.])?[0-9]+)\\s*\\,\\s*([+-]?([0-9]*[.])?[0-9]+)\\s*\\)\\s*)");
+	std::string coords{};
+	std::smatch matches;
 
-	std::cout << "u = (" << v->get_x() << ", " << v->get_y() << ", " << v->get_z() << ")" << std::endl;
-	std::cout << "v = (" << v[1].get_x() << ", " << v[1].get_y() << ", " << v[1].get_z() << ")" << std::endl;
+
+	Vector* vectors = new Vector[2];
+	Math m;
+
+	size_t k{ 0 };
+	for (size_t i = 1; i < 3; ++i)
+	{
+		std::cout << "Ingrese el " << i << " vector en el formato(#, #, #) : ";
+		getline(std::cin, coords);
+		bool match = std::regex_match(coords, matches, pattern);
+		if (match)
+		{
+			double x = std::stof(matches[2]);
+			double y = std::stof(matches[4]);
+			double z = std::stof(matches[6]);
+			vectors[k].set_y(y);
+			vectors[k].set_x(x);
+			vectors[k].set_z(z);
+			++k;
+		}
+	}
+	std::cout << "u = (" << vectors[0].get_x() << ", " << vectors[0].get_y() << ", " << vectors[0].get_z() << ")" << std::endl;
+	std::cout << "v = (" << vectors[1].get_x() << ", " << vectors[1].get_y() << ", " << vectors[1].get_z() << ")" << std::endl;
+	
+	Vector v3 = m.operator+(vectors);
 	std::cout << "La suma de los vectores es: " << "("
 		<< v3.get_x() << "i, "
 		<< v3.get_y() << "j, "
 		<< v3.get_z() << "k)"
 		<< std::endl;
-	v3 = m.operator-(v);
+	v3 = m.operator-(vectors);
 	std::cout << "La resta de los vectores es: " << "("
 		<< v3.get_x() << "i, "
 		<< v3.get_y() << "j, "
 		<< v3.get_z() << "k)"
 		<< std::endl;
-	std::cout << "La division del modulo del vector u y el modulo de v es: " << m.operator/=(v) << std::endl;
+	std::cout << "La division del modulo del vector u y el modulo de v es: " << m.operator/=(vectors) << std::endl;
 	std::cout << "Incremento en 1 cada coordenada del vector u" << "("
-		<< m.operator++(v[0].get_x()) << ", "
-		<< m.operator++(v[0].get_y()) << ", "
-		<< m.operator++(v[0].get_z()) << ")"
+		<< m.operator++(vectors[0].get_x()) << ", "
+		<< m.operator++(vectors[0].get_y()) << ", "
+		<< m.operator++(vectors[0].get_z()) << ")"
 		<< std::endl;
 	std::cout << "Incremento en 1 cada coordenada del vector v" << "("
-		<< m.operator++(v[1].get_x()) << ", "
-		<< m.operator++(v[1].get_y()) << ", "
-		<< m.operator++(v[1].get_z()) << ")"
+		<< m.operator++(vectors[1].get_x()) << ", "
+		<< m.operator++(vectors[1].get_y()) << ", "
+		<< m.operator++(vectors[1].get_z()) << ")"
 		<< std::endl;
 	std::cout << "Decremento en 1 cada coordenada del vector u" << "("
-		<< m.operator--(v[0].get_x()) << ", "
-		<< m.operator--(v[0].get_y()) << ", "
-		<< m.operator--(v[0].get_z()) << ")"
+		<< m.operator--(vectors[0].get_x()) << ", "
+		<< m.operator--(vectors[0].get_y()) << ", "
+		<< m.operator--(vectors[0].get_z()) << ")"
 		<< std::endl;
 	std::cout << "Decremento en 1 cada coordenada del vector v" << "("
-		<< m.operator--(v[1].get_x()) << ", "
-		<< m.operator--(v[1].get_y()) << ", "
-		<< m.operator--(v[1].get_z()) << ")"
+		<< m.operator--(vectors[1].get_x()) << ", "
+		<< m.operator--(vectors[1].get_y()) << ", "
+		<< m.operator--(vectors[1].get_z()) << ")"
+		<< std::endl;
+
+	Vector v4 = m.operator>(vectors);
+	std::cout << "El vector mayor eentre u y v es: " <<"("
+		<< v4.get_x() << ", "
+		<< v4.get_y() << ", "
+		<< v4.get_z() << ")"
 		<< std::endl;
 
 }
