d90d28e04ac915db2ea0796773e5447aaf5d7ccb
Theo Martin Rosero Mosquera <theomrosero@gmail.com>
2021-06-18 00:00:20 -0500

    Homework 17 Documentation&Validation

--- I-PARCIAL/Homework 17 DoubleList/ConsoleApplication4/ConsoleApplication4/ListaDobleEnlazada.h
+++ I-PARCIAL/Homework 17 DoubleList/ConsoleApplication4/ConsoleApplication4/ListaDobleEnlazada.h
@@ -1,3 +1,16 @@
+/** UNIVERSIDAD DE LAS FUERZAS ARMADAS "ESPE"
+*			INGENIERIA SOFTWARE
+*
+*AUTORES : YULLIANA ROMAN
+*JUNIOR JURADO
+*THEO ROSERO
+*ALEX PAGUAY
+*SANTIAGO SAÑAY
+*TEMA: Listas dobles
+*FECHA DE CREACION : 14 DE MAYO DEL 2021
+*FECHA DE MODIFICACION: 17 DE MAYO 2021
+*/
+
 #pragma once
 #include "Nodo.h"
 #include<functional>
@@ -6,18 +19,65 @@ using namespace std;
 class ListaDobleEnlazada
 {
 private:
+	/**
+	 * @brief 
+	 * 
+	 * @return Nodo* 
+	 */
 	Nodo* ultimo_nodo();
+
+	/**
+	 * @brief atributos de clase
+	 * 
+	 */
 	Nodo* cabeza = nullptr;
 	int tamanio = 0;
 public:
+	/**
+	 * @brief Construct a new Lista Doble Enlazada object
+	 * 
+	 */
 	ListaDobleEnlazada() = default;
+	/**
+	 * @brief insertar nodo
+	 * 
+	 */
 	void insertar(int);
+	/**
+	 * @brief insertar cabeza
+	 * 
+	 */
 	void insertar_cabeza(int);
+	/**
+	 * @brief insertar por indice
+	 * 
+	 */
 	void insertar_por_indice(int,int);
+	/**
+	 * @brief eliminar por indice
+	 * 
+	 */
 	void eliminar_por_indice(int);
+	/**
+	 * @brief eliminar cabeza
+	 * 
+	 */
 	void eliminar_cabeza();
+	/**
+	 * @brief eliminar cola
+	 * 
+	 */
 	void eliminar_cola();
+	/**
+	 * @brief imprimir
+	 * 
+	 */
 	void recorrer(function<void(int,int)>recorrido);
+	/**
+	 * @brief buscar indice
+	 * 
+	 * @return int 
+	 */
 	int consultar(int);
 
 
--- I-PARCIAL/Homework 17 DoubleList/ConsoleApplication4/ConsoleApplication4/Nodo.h
+++ I-PARCIAL/Homework 17 DoubleList/ConsoleApplication4/ConsoleApplication4/Nodo.h
@@ -1,20 +1,66 @@
+/** UNIVERSIDAD DE LAS FUERZAS ARMADAS "ESPE"
+*			INGENIERIA SOFTWARE
+*
+*AUTORES : YULLIANA ROMAN
+*JUNIOR JURADO
+*THEO ROSERO
+*ALEX PAGUAY
+*SANTIAGO SAÑAY
+*TEMA: Listas dobles
+*FECHA DE CREACION : 14 DE MAYO DEL 2021
+*FECHA DE MODIFICACION: 17 DE MAYO 2021
+*/
+
 #pragma once
 class Nodo
 {
 private:
+	/**
+	 * @brief atributos de clase 
+	 * 
+	 */
 	int valor;
 	Nodo* siguiente = nullptr;
 	Nodo* anterior = nullptr;
 public:
+	/**
+	 * @brief Construct a new Nodo object
+	 * 
+	 */
 	Nodo(int);
+	/**
+	 * @brief Get the siguiente object
+	 * 
+	 * @return Nodo* 
+	 */
 	Nodo* get_siguiente(void);
+	/**
+	 * @brief Get the anterior object
+	 * 
+	 * @return Nodo* 
+	 */
 	Nodo* get_anterior(void);
+	/**
+	 * @brief Set the siguiente object
+	 * 
+	 */
 	void set_siguiente(Nodo*);
+	/**
+	 * @brief Set the anterior object
+	 * 
+	 */
 	void set_anterior(Nodo*);
+	/**
+	 * @brief Get the valor object
+	 * 
+	 * @return int 
+	 */
 	int get_valor(void);
+	/**
+	 * @brief Set the valor object
+	 * 
+	 */
 	void set_valor(int);
-
-
 
 };
 
