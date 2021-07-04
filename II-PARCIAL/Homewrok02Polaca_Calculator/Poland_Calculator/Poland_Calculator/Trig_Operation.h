67649045653be97a813ca9cba32bd024c2400e99
Alex Santiago Paguay Flores <75266819+alexpaguay1991@users.noreply.github.com>
2021-07-04 11:32:51 -0500

    "polaca calculator"

---
+++ II-PARCIAL/Homewrok02Polaca_Calculator/Poland_Calculator/Poland_Calculator/Trig_Operation.h
@@ -0,0 +1,62 @@
+﻿/** UNIVERSIDAD DE LAS FUERZAS ARMADAS "ESPE"
+*			INGENIERIA SOFTWARE
+*
+*
+*@author THEO ROSERO
+*@author YULLIANA ROMAN
+*@author JUNIOR JURADO
+*@author ALEX PAGUAY
+*@author SANTIAGO SA�AY
+*TEMA: FUNCIONES TRIGONOMETRICAS
+*FECHA DE CREACION : 5 DE JUNIO DE 2021
+*FECHA DE MODIFICACION: 6 DE JUNIO 2021
+*/
+#pragma once
+
+class Trig_Operation
+{
+public:
+	/**
+ 	* @brief calculate sine with taylor series
+ 	* 
+ 	* @return double 
+ 	*/
+	double sin(double);
+	
+	/**
+	 * @brief calculate contangent with taylor series
+	 * 
+	 * @return double 
+	 */
+	double cot(double);
+	
+	/**
+	 * @brief calculate cosine with taylor series
+	 * 
+	 * @return double 
+	 */
+	double cos(double);
+	
+	/**
+	 * @brief calculate tangent with taylor series
+	 * 
+	 * @return double 
+	 */
+	double tan(double);
+	
+	/**
+	 * @brief calculate tangent with taylor series
+	 * 
+	 * @return double 
+	 */
+	double sec(double);
+	
+	/**
+	 * @brief calculate factorial with recursive function 
+	 * 
+	 * @return long 
+	 */
+	long factorial(int);
+	double potencia(double, double);
+};
+
