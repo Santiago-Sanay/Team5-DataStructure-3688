3f02c75c5abf0e0ccb8894c1f4fa45a4f765eb49
Alex Santiago Paguay Flores <75266819+alexpaguay1991@users.noreply.github.com>
2021-07-05 08:54:13 -0500

    "uodate poland calculator"

--- II-PARCIAL/Homewrok02Polaca_Calculator/Poland_Calculator/Poland_Calculator/Poland_Calculator.cpp
+++ II-PARCIAL/Homewrok02Polaca_Calculator/Poland_Calculator/Poland_Calculator/Poland_Calculator.cpp
@@ -13,18 +13,22 @@ int main()
    
     mystring str;
     string num = "12.36";
-    cout << str.stod(num) << endl;
+    //cout << str.stod(num) << endl;
     Stack<string> datos;
     datos = op.ingresar_datos();
+    cout << "INFIJA         :   ";
     datos.mostrar_expresion();
     Stack<string> prefija;
     Stack<string> postfija;
    
     prefija = op.infix_to_prefix1(datos);
+    cout << "PREFIJA        :   ";
     prefija.mostrar_expresion();
     
     postfija = op.infix_to_postfix1(datos);
+    cout << "POSTFIJA       :   ";
     postfija.mostrar_expresion();
+    cout << "RESULTADO      :   ";
     cout << op.calculate(prefija) << endl;
    
     std::cout << "Hello World!\n";
