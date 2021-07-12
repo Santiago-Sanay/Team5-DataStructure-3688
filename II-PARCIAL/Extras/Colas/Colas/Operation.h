b0d028e34dae2584b8046ce485ddc714a802c00f
YullianaRoman <yulianaroman376@gmail.com>
2021-07-11 21:10:41 -0500

    Lambda Functions

--- II-PARCIAL/Extras/Colas/Colas/Operation.h
+++ II-PARCIAL/Extras/Colas/Colas/Operation.h
@@ -7,10 +7,10 @@ class Operation
 {
 public:
 	Client first();
-	Queue<Client> generate_table(int);
-	Queue<Client> calculate_arrival_time(Queue<Client>&,int);
+	Queue<Client> generate_table(int); 
+	Queue<Client> calculate_arrival_time(function<void(int&, Node<Client>*, Client&, int&)>assign, Queue<Client>&, int);
+	Queue<Client> calculate_service_time(function<void(int&, Queue<Client>, Client&, Queue<Client>&, int&)>assign, Queue<Client>& clients, int index);
 	void mostrar(Queue<Client>);
-	Queue<Client> calculate_service_time(Queue<Client>&,int);
 	Queue<Client> calculate_waiting_time(Queue<Client>&,int);
 	int calculate_exit_time(Client);
 	void calculate_summary(Queue<Client>&);
