ef820d06427510031d1f4c87cf6a8579af5ce6f0
Alex Santiago Paguay Flores <75266819+alexpaguay1991@users.noreply.github.com>
2021-07-27 00:01:10 -0500

    "update"

--- III-PARCIAL/English Spanish Translator/English Spanish Translator/Operacion.h
+++ III-PARCIAL/English Spanish Translator/English Spanish Translator/Operacion.h
@@ -1,9 +1,39 @@
 #pragma once
 #include <fstream>
+#include "avl_tree.h"
+#include "mystring.h"
+#include <iostream>
 using namespace std;
 class Operacion
 {
 public:
 	void cargar_Archivo_en_arbol(AvlTree&, string);
 
+	
 };
+void Operacion::cargar_Archivo_en_arbol(AvlTree& tree, string file)
+{
+
+
+	ifstream archivo(file.c_str(), ios::in);
+	if (archivo.fail()) {
+		cout << "no se pudo abrir el archivo";
+		return;
+	}
+	mystring str;
+	string linea;
+	string spanish;
+	string english;
+	int separate = 0;
+	while (!archivo.eof()) {
+		getline(archivo, linea);
+		separate = str.find(linea, "\t", 0);
+		english = str.substract(linea, 0, separate);
+		spanish = str.substract(linea, separate + 1, str.length(linea) - separate - 1);
+		tree.insert(spanish, english);
+		/*cout << spanish << endl;
+		cout << english << endl;*/
+	}
+	archivo.close();
+
+}
--- III-PARCIAL/English Spanish Translator/English Spanish Translator/main.cpp
+++ III-PARCIAL/English Spanish Translator/English Spanish Translator/main.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
-#include "avl_tree.h"
-#include "avl_node.h"
+
 #include "utils.h"
 #include "Operacion.h"
 #include <iomanip>
@@ -9,18 +8,35 @@ int main()
 {
     Operacion op;
     AvlTree tree;
-    op.cargar_Archivo_en_arbol(tree, "dictionary.txt");
+    int a = 0;
+    op.cargar_Archivo_en_arbol(tree, "dic.txt");
     //? tree.insert(spanish_word, english_word);
-    /*tree.insert("Hola", "Hello");
+    tree.insert("Hola", "Hello");
     tree.insert("sol", "sun");
     tree.insert("inteligente", "smart");
     tree.insert("telefono", "phone");
     tree.insert("casa", "house");
+    int i = 1;
+    string f2 = "depuradodic.txt";
+    ofstream archivo2(f2.c_str());
+    tree.for_each_inorder([&i,&archivo2](AvlNode* node)
+        { std::cout << i++ << "  " << node->english_word() << ", " << node->spanish_word() << std::endl;
+    
+        /*if (i == 1) {
+            archivo2 << node->english_word() << ", " << node->spanish_word();
+        }*/
+        //else {
+   
+            archivo2 << endl;
+            archivo2 << node->english_word() << "," << node->spanish_word();
+       // }
+        });
+    archivo2.close();
 
     std::cout << "Elementos Insertados" << std::endl;
 
     std::cout << "\nInorder Tree" << std::endl;
-    int i = 1;
+     i = 1;
 
     std::cout << std::endl;
     tree.print_tree(tree.get_root(), nullptr, false, 1);
@@ -39,6 +55,6 @@ int main()
     std::cout << "Nodo encontrado: " << test_find->spanish_word() << "," << test_find->english_word() << std::endl;
 
     tree.print_tree(tree.get_root(), nullptr, false, 0);
-    */
+    
     return 0;
 }
